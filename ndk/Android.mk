d082fa0fcd3adb3214552a80215b1a008b73ebdc
iamlooper <iamloooper@gmail.com>
2023-05-13 13:36:33 +0500

    feat: ndk changes - enable exceptions x2 - remove `-fno-rtti` flag because RTTI is disabled by default in `ndk-build`

--- ndk/Android.mk
+++ ndk/Android.mk
@@ -3,6 +3,7 @@ LOCAL_PATH := $(call my-dir)/src
 include $(CLEAR_VARS)
 LOCAL_MODULE := android_enhancer
 LOCAL_SRC_FILES := android_enhancer.cpp
+LOCAL_CPP_FEATURES += exceptions 
 include $(BUILD_EXECUTABLE)
 
 include $(CLEAR_VARS)
